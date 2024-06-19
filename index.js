9601f673a71afaadfdbb41107b06256d51e6b253
Shanmukh459 <saikrrish459@gmail.com>
2024-06-19 11:45:52 -0700

    shoe prime numbers

--- index.js
+++ index.js
@@ -95,3 +95,19 @@ function showStars(rows) {
   }
 }
 showStars(5)
+
+// show primes
+function showPrimes(limit) {
+  for(let i = 2; i <= limit; i++){
+    if(isPrime(i)) console.log(i)
+  }
+}
+
+function isPrime(number){
+    for(let j = 2; j < number ; j++)
+      if(number%j === 0) {
+        return false
+      }
+  return true
+}
+showPrimes(20)
