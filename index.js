9ad5a0bb62e8efc529addcc468ecda9d7cd565d4
Shanmukh459 <saikrrish459@gmail.com>
2024-06-19 11:08:14 -0700

    fizzbuzz

--- index.js
+++ index.js
@@ -13,4 +13,18 @@ function isLandsscape(width, height) {
   return width > height
 }
 
-isLandsscape(300, 500)
+console.log(isLandsscape(300, 500))
+
+// FizzBuzz
+
+function fizzBuzz(input) {
+  if(typeof input !== "number") {
+    return "Not a Number"
+  }
+  if(input % 3 === 0 && input%5 === 0) return "FizzBuzz"
+  else if(input % 3 === 0) return "Fizz"
+  else if(input % 5 === 0) return "Buzz"
+  else return input
+}
+
+console.log(fizzBuzz(false))
